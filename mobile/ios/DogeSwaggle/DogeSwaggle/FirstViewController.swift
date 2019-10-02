7d4c20324463bd94eee745bb1f5fbb0a84dd49b9
Aaron <ajjubbal@gmail.com>
2019-10-02 16:18:51 -0700

    Created iOS Swift project for DogeSwaggle.

---
+++ mobile/ios/DogeSwaggle/DogeSwaggle/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  DogeSwaggle
+//
+//  Created by Aaron Jubbal on 10/2/19.
+//  Copyright © 2019 Tech Crunch 2019 Table 34. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ mobile/ios/DogeSwaggle/DogeSwaggle/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  DogeSwaggle
+//
+//  Created by Aaron Jubbal on 10/2/19.
+//  Copyright © 2019 Tech Crunch 2019 Table 34. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
