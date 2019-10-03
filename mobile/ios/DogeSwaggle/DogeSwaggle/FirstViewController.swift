aebd5440ebba8ff245b44d1d4cfc27d14b971aa4
Aaron <ajjubbal@gmail.com>
2019-10-02 17:34:24 -0700

    Blank undismissable modal appears upon first launch.

--- mobile/ios/DogeSwaggle/DogeSwaggle/FirstViewController.swift
+++ mobile/ios/DogeSwaggle/DogeSwaggle/FirstViewController.swift
@@ -13,6 +13,21 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        guard let dogType = UserDefaults.standard.string(forKey: "dogType") else {
+            let viewController = DogeSelectionViewController(nibName: "DogeSelectionViewController", bundle: nil)
+            if #available(iOS 13.0, *) {
+                viewController.isModalInPresentation = true
+            }
+            self.present(viewController, animated: true, completion: nil)
+            return;
+        }
     }
 
 
